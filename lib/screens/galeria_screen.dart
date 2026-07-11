b9f4dd5f73cf053082822f85be4b1f72af261fe5
AlexdanTB <alexdanieltb220@gmail.com>
2026-07-11 08:06:35 -0500

    image picker

--- lib/screens/galeria_screen.dart
+++ lib/screens/galeria_screen.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GaleriaScreen extends StatelessWidget {
   const GaleriaScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [Imagen()]),
+    );
+  }
+}
+
+/////
+
+class Imagen extends StatefulWidget {
+  const Imagen({super.key});
+
+  @override
+  State<Imagen> createState() => _ImagenState();
+}
+
+class _ImagenState extends State<Imagen> {
+  XFile? foto;
+
+  void actualizarFoto(XFile nuevaFoto) {
+    setState(() {
+      foto = nuevaFoto;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
